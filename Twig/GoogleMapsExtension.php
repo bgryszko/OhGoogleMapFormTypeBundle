701c24d5987e253df5fb5eebb33fb96e8873f76d
Bart <bartosz.gryszko@gmail.com>
2014-01-12 13:05:49 +0100

    Twig function rendering scripts

--- Twig/GoogleMapsExtension.php
+++ Twig/GoogleMapsExtension.php
@@ -4,18 +4,20 @@ namespace Oh\GoogleMapFormTypeBundle\Twig;
 
 class GoogleMapsExtension extends \Twig_Extension
 {
-    public function getFilters()
+    public function getFunctions()
     {
         return array(
-            'google_maps_scripts'  => new \Twig_Filter_Method($this, 'renderGoogleMapsScripts'),
+            'google_maps_scripts'  => new \Twig_Function_Method($this, 'renderGoogleMapsScripts', array('needs_environment' => true, 'is_safe' => array('html')))
         );
     }
 
-    public function renderGoogleMapsScripts()
+    public function renderGoogleMapsScripts(\Twig_Environment $environment, $formFiled)
     {
-        return "aaaaa";
-    }
 
+    	return $environment->render('OhGoogleMapFormTypeBundle:Form:scripts.html.twig', $formFiled->vars);
+
+    }
+    
     public function getName()
     {
         return 'google_maps_extension';
