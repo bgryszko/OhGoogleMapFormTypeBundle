05bf51b65f82d858f42e9962eeb678bc11d7575e
Bart <bartosz.gryszko@gmail.com>
2014-01-12 12:21:35 +0100

    Moved scripts rendering to extension

---
+++ Twig/GoogleMapsExtension.php
@@ -0,0 +1,21 @@
+namespace Oh\GoogleMapFormTypeBundle\Twig;
+
+class GoogleMapsExtension extends \Twig_Extension
+{
+    public function getFilters()
+    {
+        return array(
+            new \Twig_Filter_Method('google_maps_scripts', array($this, 'renderGoogleMapsScripts')),
+        );
+    }
+
+    public function renderGoogleMapsScripts()
+    {
+        return "aaaaa";
+    }
+
+    public function getName()
+    {
+        return 'google_maps_extension';
+    }
+}
